61d72e8b4fde17d0932ca64e3191dc19c4f64a64
Kira McLean <email@kiramclean.com>
2024-04-29 15:56:03 -0300

    cleaning up household examples

--- london_clojurians_april_2024/src/notebooks/prepared.clj
+++ london_clojurians_april_2024/src/notebooks/prepared.clj
@@ -1,7 +1,9 @@
 (ns notebooks.prepared
   (:require
    [notebooks.hana :as hana]
-   [tablecloth.api :as tc]))
+   [tablecloth.api :as tc]
+   [tech.v3.dataset :as ds]
+   [tech.v3.dataset.modelling :as modelling]))
 
 ;; # Clojure for Data Deep Dive
 ;; London Clojurians April 2024
@@ -23,9 +25,11 @@
 ;; A walkthrough of a realistic ML workflow using an example dataset about housing prices.
 
 (def housing
-  (tc/dataset "data/iowa-data.csv"))
+  (tc/dataset "data/housing-data.csv"))
 
 (tc/column-names housing)
+
+;; ### Outliers
 
 ;; Plot outliers so we can remove them ([as recommended by the author](http://jse.amstat.org/v19n3/decock.pdf) of the original dataset)
 
@@ -40,12 +44,37 @@
     (hana/plot {:X "GrLivArea" :Y "SalePrice"})
     (hana/layer-point))
 
-;; Initial processing
+(def without-outliers
+  (-> housing
+      (tc/drop-rows #(> (% "GrLivArea") 4000))))
 
-(-> housing
-    )
+;; ## Processing
 
-;; (-> housing
-;;     (tc/drop-rows #(> (% "GrLivArea") 4000))
+;; ### Removing missing values
+;; First find columns that have missing values.
 
-;;     )
+(-> without-outliers
+    tc/info
+    (tc/select-rows #(< 0 (:n-missing %)))
+    :col-name)
+
+;; Replace missing values with sensible alternative ones
+
+(def no-missing-values
+  (-> without-outliers
+      (tc/replace-missing ["LotFrontage" "MasVnrArea"] 0)
+      (tc/replace-missing ["Alley" "MasVnrType"] "None")
+      (tc/replace-missing ["BsmtQual" "BsmtCond" "BsmtExposure" "BsmtFinType1" "BsmtFinType2"
+                           "Fence" "FireplaceQu" "GarageType" "GarageFinish" "GarageYrBlt"
+                           "GarageQual" "GarageCond" "MiscFeature" "PoolQC"] "No")))
+
+;; Convert string columns to numeric ones so they can be handled by our models. Finding all string columns, and then assigning them numeric values.
+
+(let [string-col-names (-> no-missing-values
+                           tc/info
+                           (tc/select-rows #(= :string (:datatype %)))
+                           :col-name)]
+  (-> no-missing-values
+      (ds/categorical->number string-col-names)))
+
+(without-outliers "MoSold")
