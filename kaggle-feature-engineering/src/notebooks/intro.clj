e4e11c706ba3b4812901490d184e9fe3f405525b
Kira McLean <email@kiramclean.com>
2024-06-04 14:05:33 -0300

    add example of computing loss with and without engineered features

--- kaggle-feature-engineering/src/notebooks/intro.clj
+++ kaggle-feature-engineering/src/notebooks/intro.clj
@@ -1,5 +1,11 @@
 (ns notebooks.intro
-  (:require [tablecloth.api :as tc]))
+  (:require
+   [scicloj.metamorph.ml :as ml]
+   [scicloj.metamorph.ml.loss :as loss]
+   [scicloj.ml.smile.regression]
+   [tablecloth.api :as tc]
+   [tablecloth.column.api :as tcc]
+   [tech.v3.dataset.modelling :as dsmod]))
 
 ;; The task for this dataset is to predict a concrete's compressive strength given its formulation.
 
@@ -8,16 +14,27 @@
 
 (tc/head concrete)
 
-(def X (tc/drop-columns concrete ["CompressiveStrength"]))
+;; Get a baseline error with the dataset itself
+(let [
+      prediction-target "CompressiveStrength"
+      ds (-> concrete
+             (dsmod/set-inference-target prediction-target))
+      model (ml/train ds {:model-type :smile.regression/random-forest})
+      predictions (ml/predict ds model)]
+  (loss/mae (ds prediction-target) (predictions prediction-target)))
 
-(def y (tc/select-columns concrete ["CompressiveStrength"]))
+;; Add synthetic features and re-check the error
+(let [prediction-target "CompressiveStrength"
+      with-ratios (-> concrete
+                      (tc/map-columns "FCRatio" ["FineAggregate" "CoarseAggregate"] tcc//)
+                      (tc/map-columns "AggCmtRatio"
+                                      ["CoarseAggregate" "FineAggregate" "Cement"]
+                                      (fn [coarse fine cement]
+                                        (tcc// (tcc/+ coarse fine) cement)))
+                      (tc/map-columns "WtrCmtRatio" ["Water" "Cement"] tcc//)
+                      (dsmod/set-inference-target prediction-target))
+      model (ml/train with-ratios {:model-type :smile.regression/random-forest})
+      predictions (ml/predict with-ratios model)]
+  (loss/mae (with-ratios prediction-target) (predictions prediction-target)))
 
-;; (def baseline = )
-
-;; baseline = RandomForestRegressor(criterion="absolute_error", random_state=0)
-;; baseline_score = cross_val_score(
-;;     baseline, X, y, cv=5, scoring="neg_mean_absolute_error"
-;; )
-;; baseline_score = -1 * baseline_score.mean()
-
-;; print(f"MAE Baseline Score: {baseline_score:.4}")
+;; See that the error is better with our engineered features included
