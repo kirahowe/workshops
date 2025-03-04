eee433628f79210a81938750434e435dc7900de5
Kira Howe <email@kirahowe.com>
2025-03-04 00:42:47 -0400

    work on getting started guide

--- bobkonf_2025/bin/build.clj
+++ bobkonf_2025/bin/build.clj
@@ -17,9 +17,11 @@
                :base-source-path "src"
                :base-target-path output-dir
                :subdirs-to-sync ["src" "data"]
-               :source-path ["index.clj" "notebooks/1_installation_guide.clj"] #_(->> (io/file "src/notebooks")
-                                                                                      file-seq
-                                                                                      (filter #(str/ends-with? % "clj"))
-                                                                                      sort)
+               :source-path ["index.clj"
+                             "notebooks/1_installation_guide.clj"
+                             "notebooks/2_getting_started_with_clojure.clj"] #_(->> (io/file "src/notebooks")
+                                                                          file-seq
+                                                                          (filter #(str/ends-with? % "clj"))
+                                                                          sort)
                :clean-up-target-dir true})
   (System/exit 0))
